e231daa14337ab1aca48f3f3f64ab165f019c488
thish <thevarajah@l3s.de>
2017-05-29 18:42:41 +0200

    DTO file implemented

--- src/main/java/io/flightlog/dto/FlightLog.java
+++ src/main/java/io/flightlog/dto/FlightLog.java
@@ -1,5 +1,93 @@
 package io.flightlog.dto;
 
 public class FlightLog {
-
+	
+	private double sNo;
+	private double pm_iAddrModeS;
+	private double pm_dLatitude;
+	private double pm_dLongitude;
+	private double pm_dAltitudeMSL;
+	private double pm_dRoll;
+	private double pm_dPitch;
+	private double pm_dHeading;
+	private double pm_dGroundTrack;
+	private double pm_dAirSpeed;
+	private double pm_dGroundSpeed;
+	private double pm_dVerticalSpeed;
+	
+	
+	public double getsNo() {
+		return sNo;
+	}
+	public void setsNo(double sNo) {
+		this.sNo = sNo;
+	}
+	public double getPm_iAddrModeS() {
+		return pm_iAddrModeS;
+	}
+	public void setPm_iAddrModeS(double pm_iAddrModeS) {
+		this.pm_iAddrModeS = pm_iAddrModeS;
+	}
+	public double getPm_dLatitude() {
+		return pm_dLatitude;
+	}
+	public void setPm_dLatitude(double pm_dLatitude) {
+		this.pm_dLatitude = pm_dLatitude;
+	}
+	public double getPm_dLongitude() {
+		return pm_dLongitude;
+	}
+	public void setPm_dLongitude(double pm_dLongitude) {
+		this.pm_dLongitude = pm_dLongitude;
+	}
+	public double getPm_dAltitudeMSL() {
+		return pm_dAltitudeMSL;
+	}
+	public void setPm_dAltitudeMSL(double pm_dAltitudeMSL) {
+		this.pm_dAltitudeMSL = pm_dAltitudeMSL;
+	}
+	public double getPm_dRoll() {
+		return pm_dRoll;
+	}
+	public void setPm_dRoll(double pm_dRoll) {
+		this.pm_dRoll = pm_dRoll;
+	}
+	public double getPm_dPitch() {
+		return pm_dPitch;
+	}
+	public void setPm_dPitch(double pm_dPitch) {
+		this.pm_dPitch = pm_dPitch;
+	}
+	public double getPm_dHeading() {
+		return pm_dHeading;
+	}
+	public void setPm_dHeading(double pm_dHeading) {
+		this.pm_dHeading = pm_dHeading;
+	}
+	public double getPm_dGroundTrack() {
+		return pm_dGroundTrack;
+	}
+	public void setPm_dGroundTrack(double pm_dGroundTrack) {
+		this.pm_dGroundTrack = pm_dGroundTrack;
+	}
+	public double getPm_dAirSpeed() {
+		return pm_dAirSpeed;
+	}
+	public void setPm_dAirSpeed(double pm_dAirSpeed) {
+		this.pm_dAirSpeed = pm_dAirSpeed;
+	}
+	public double getPm_dGroundSpeed() {
+		return pm_dGroundSpeed;
+	}
+	public void setPm_dGroundSpeed(double pm_dGroundSpeed) {
+		this.pm_dGroundSpeed = pm_dGroundSpeed;
+	}
+	public double getPm_dVerticalSpeed() {
+		return pm_dVerticalSpeed;
+	}
+	public void setPm_dVerticalSpeed(double pm_dVerticalSpeed) {
+		this.pm_dVerticalSpeed = pm_dVerticalSpeed;
+	}
+	
+	
 }
