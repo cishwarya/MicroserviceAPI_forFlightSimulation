405827c6bc1d61815eb82215cc15f3e6c91e63a6
thish <thevarajah@l3s.de>
2017-07-07 00:11:09 +0200

    cors origin added

--- src/main/java/io/flightlog/controller/Controller.java
+++ src/main/java/io/flightlog/controller/Controller.java
@@ -2,11 +2,7 @@ package io.flightlog.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -35,6 +31,7 @@ public class Controller {
 	 * @return the response entity
 	 */
 	/* curl -X GET http://localhost:8080/get/0.03/ */
+	@CrossOrigin
 	@RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Object> get(@PathVariable String id){
 		try {			
@@ -68,6 +65,7 @@ public class Controller {
 	 * @return the response body builder
 	 */
 	/* curl -H "Content-Type: application/json" -X POST -d '{"sNo":"0.03", "pm_iAddrModeS":"2", "pm_dLatitude":"52.308928", "pm_dLongitude":"10.373013", "pm_dAltitudeMSL":"500.000000","pm_dRoll": "0.001500", "pm_dPitch":"0.180390", "pm_dHeading":"1.396260", "pm_dGroundTrack":"1.444650", "pm_dAirSpeed":"66.902228", "pm_dGroundSpeed":"92.341750", "pm_dVerticalSpeed":"0.005334"}' http://localhost:8080/post */
+	@CrossOrigin
 	@RequestMapping(value = "/post", method = RequestMethod.POST)
 	public ResponseEntity<String> post(@RequestBody FlightLog log){
 		try {
